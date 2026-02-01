336b5ef4fe1d6157392b2a0edf790e8f85710342
Prankapple <bnnaadcscs@gmail.com>
2026-02-01 14:26:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
   startButton.addEventListener("click", async () => {
     if (started) return;
     started = true;
-
+    requestFullscreen()
     startButton.style.display = "none";
+    // Get viewport size
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    // Get logo size
+    const rect = logo.getBoundingClientRect();
+
+    // Center logo dynamically
+    logo.style.position = "fixed";
+    logo.style.left = (vw - rect.width) / 2 + "px";
+    logo.style.top  = (vh - rect.height) / 2 + "px";
 
     try {
       const res = await fetch("src/lines.txt", { cache: "no-store" });
@@ -44,6 +55,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     setTimeout(appendNextLine, 80);
   }
+  function requestFullscreen() {
+  const elem = document.documentElement; // whole page
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { // Safari
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE11
+    elem.msRequestFullscreen();
+  }
+}
+
 });
+
 
 
